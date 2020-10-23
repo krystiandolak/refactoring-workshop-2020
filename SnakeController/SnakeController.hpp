1e873aea1651cba26993d95f32e0d6227f780c5e
Krystian Dolak <krystian.dolak@gmail.com>
2020-10-23 13:47:01 +0200

    changing the scope of functions

--- SnakeController/SnakeController.hpp
+++ SnakeController/SnakeController.hpp
@@ -24,24 +24,11 @@ struct UnexpectedEventException : std::runtime_error
 
 class Controller : public IEventHandler
 {
-    struct Segment;
 public:
     Controller(IPort& p_displayPort, IPort& p_foodPort, IPort& p_scorePort, std::string const& p_config);
 
     Controller(Controller const& p_rhs) = delete;
     Controller& operator=(Controller const& p_rhs) = delete;
-
-    void isGameLost(Segment& head, bool& isLost);
-    void isHeadOnFruit();
-    void snakeOutOfMap(bool& isLost);
-    void setFreeLastSegment();
-    void nextMoveSnake(Segment& head);
-
-    bool isReceivedFoodcolidedWithSnake(EventT<FoodInd> const& food);
-    bool isRequestedFoodcolidedWithSnake(EventT<FoodResp> const& food);
-    void placeNewFoodInd(EventT<FoodInd> const& food);
-    void placeNewFoodResp(EventT<FoodResp> const& food);
-    void clearOldFood();
 
     void receive(std::unique_ptr<Event> e) override;
 
@@ -62,6 +49,18 @@ private:
 
     Direction m_currentDirection;
     std::list<Segment> m_segments;
+
+    void isGameLost(Segment& head, bool& isLost);
+    void isHeadOnFruit();
+    void snakeOutOfMap(bool& isLost);
+    void setFreeLastSegment();
+    void nextMoveSnake(Segment& head);
+
+    bool isReceivedFoodcolidedWithSnake(EventT<FoodInd> const& food);
+    bool isRequestedFoodcolidedWithSnake(EventT<FoodResp> const& food);
+    void placeNewFoodInd(EventT<FoodInd> const& food);
+    void placeNewFoodResp(EventT<FoodResp> const& food);
+    void clearOldFood();
 };
 
 } // namespace Snake
